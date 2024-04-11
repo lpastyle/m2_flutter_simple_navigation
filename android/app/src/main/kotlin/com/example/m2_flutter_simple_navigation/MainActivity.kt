4a196490de41be0ad7551c0ea0f793c2b611db77
lpastyle <lpastorelli@lpastyle.com>
2024-04-11 18:11:39 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/m2_flutter_simple_navigation/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.m2_flutter_simple_navigation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/home_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: 
+            Text(
+              'THIS THE HOME PAGE',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+      ),
+    );
+  }
+}
